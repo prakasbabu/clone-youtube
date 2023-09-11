de913219323020cb653320383f0efb0bb7574fe2
Prakash Aryal <prakasbabuaryal@gmail.com>
2023-09-11 18:12:56 -0400

    sfgsdf

--- backend/src/main/java/com/prakash/controller/VideoController.java
+++ backend/src/main/java/com/prakash/controller/VideoController.java
@@ -6,13 +6,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
+
 @RestController
 @RequestMapping("/api/videos")
 @RequiredArgsConstructor
 public class VideoController {
 
     private final VideoService videoService;
-    @PostMapping("/upload")
+    @PostMapping(path = "/upload", consumes = {MULTIPART_FORM_DATA_VALUE})
     @ResponseStatus(HttpStatus.CREATED)
     public void uploadVideo(@RequestParam("file") MultipartFile file){
         System.out.println("in the controller");
